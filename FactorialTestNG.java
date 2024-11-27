9525bf7ef4a520c86b53d1dc727597a9dff00161
NurievAlmaz <nurievalm5@gmail.com>
2024-11-27 19:45:24 +0300

    Add files via upload

---
+++ FactorialTestNG.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class FactorialTestNG {
+
+    @Test
+    public void testFactorialOfZero() {
+        assertEquals(Factorial.calculateFactorial(0), 1);
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        assertEquals(Factorial.calculateFactorial(4), 120);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        Factorial.calculateFactorial(-54);
+    }
+}
